72bc951320ee3930e196c63ff9bbec43f6efd16f
Sabine Reisser <16286888+sreisser@users.noreply.github.com>
2021-12-23 12:38:24 +0100

    creating empty data folder for tables.xml

--- webserver/anaspingpong/__init__.py
+++ webserver/anaspingpong/__init__.py
@@ -1,11 +1,12 @@
 import os
-
 from flask import Flask
-
+from pathlib import Path
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    Path("anaspingpong/data").mkdir(exist_ok=True)
     app.config.from_mapping(
      #   SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'anaspingpong.sqlite'),
